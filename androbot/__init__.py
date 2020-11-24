552822b5a80f9ec7da3a357211b2bdba3545292b
rzabolotin <rzabolotin@gmail.com>
2020-11-24 10:59:49 +1000

    исправил наименование handlers.py доработал Dockerfile для установки postgres

--- androbot/__init__.py
+++ androbot/__init__.py
@@ -1,5 +1,5 @@
 from androbot.error_handlers import *  # noqa F403
-from androbot.handles import *  # noqa F403
+from androbot.handlers import *  # noqa F403
 from androbot.main import dp, main
 
 if __name__ == "__main__":
