04236f1b8111ef27366deac931f74c64c3c1a9a2
rzabolotin <rzabolotin@gmail.com>
2020-11-26 09:54:19 +1000

    сделал render_message с исползованием питоновской функции format

--- androbot/templates.py
+++ androbot/templates.py
@@ -1,8 +1,6 @@
-from typing import Union
-
 import aiogram.types as aiotypes
 
 
-def render_message(template_text: str, message: Union[aiotypes.Message, aiotypes.CallbackQuery]):
-    parsed_text = template_text.replace("{{username}}", message.from_user.username)
+def render_message(template_text: str, message: aiotypes.Message):
+    parsed_text = template_text.format(username=message.from_user.username)
     return parsed_text
