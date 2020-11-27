2b1c82c19f6023f1d690290bc76456ae13fa8224
rzabolotin <rzabolotin@gmail.com>
2020-11-27 14:33:55 +1000

    добавил strip чтобы удалить лишние пробелы, если они будут в шаблонах (лишнии переводы строк)

--- androbot/templates.py
+++ androbot/templates.py
@@ -6,5 +6,5 @@ def render_message(template_text: str, message: aiotypes.Message):
     Функция заменяет параметры в шаблоне текста на реальные значения
     """
     values = dict(username=message.from_user.username)
-    parsed_text = template_text.format(**values)
+    parsed_text = template_text.strip().format(**values)
     return parsed_text
