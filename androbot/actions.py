f3f395a446eab12e16e30ee75d04395cd61d9ccb
v.shepard <v.shepard@just-ai.com>
2020-11-29 13:12:27 +0300

    black reformate

--- androbot/actions.py
+++ androbot/actions.py
@@ -4,6 +4,6 @@ from .specialty import Specialty
 
 
 def get_main_menu():
-    with codecs.open("welcome.message", "r", encoding='utf-8') as file:
+    with codecs.open("welcome.message", "r", encoding="utf-8") as file:
         welcome_message = file.read()
         return [welcome_message, [e.value for e in Specialty]]
--- androbot/tests.py
+++ androbot/tests.py
@@ -5,6 +5,9 @@ from androbot.actions import get_main_menu
 
 class Test(TestCase):
     def test_get_main_menu(self):
-        welcome_message = "Привет, я помогу тебе подготовиться к собеседованию\r\nВыбери " \
-                          "специальность:\r\n`Android`\r\n```print(\"Hello world\")```"
-        self.assertEqual(get_main_menu(), [welcome_message, ['android']])
+        welcome_message = (
+            "Привет, я помогу тебе подготовиться к собеседованию\r\nВыбери "
+            'специальность:\r\n`Android`\r\n```print("Hello world")``` '
+        )
+
+        self.assertEqual(get_main_menu(), [welcome_message, ["android"]])
