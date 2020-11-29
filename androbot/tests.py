8ec3a65ecebd1f40e768e1313a6de2f43d52f476
v.shepard <v.shepard@just-ai.com>
2020-11-29 12:40:23 +0300

    code style check

--- androbot/tests.py
+++ androbot/tests.py
@@ -5,5 +5,6 @@ from androbot.actions import get_main_menu
 
 class Test(TestCase):
     def test_get_main_menu(self):
-        self.assertEqual(get_main_menu(), ["Привет, я помогу тебе подготовиться к собеседованию\r\nВыбери "
-                                           "специальность:\r\n`Android`\r\n```print(\"Hello world\")```", ['android']])
+        welcome_message = "Привет, я помогу тебе подготовиться к собеседованию\r\nВыбери " \
+                          "специальность:\r\n`Android`\r\n```print(\"Hello world\")```"
+        self.assertEqual(get_main_menu(), [welcome_message, ['android']])
