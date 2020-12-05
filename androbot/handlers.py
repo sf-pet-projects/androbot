50b7b977ba4e2cb8819a7a027aa9b8252d4e52a5
Roman Zabolotin <rzabolotin@gmail.com>
2020-12-06 00:43:13 +1000

    Реализовать view специализации Android Developer (#7)
    * исправил наименование handlers.py
    доработал Dockerfile для установки postgres
    
    * добавил первые хендлеры
    
    * добавил папку templates в Dockerfile
    
    * доработал метод adduser, чтобы он рандомно вызывал исключение UserExistsError
    Добавил метод get_hello_message
    заменил inlinekeyboard на простую клавиатуру
    
    * сделал render_message с исползованием питоновской функции format
    
    * Выделил класс View в отдельный файл types.py
    убрал Профиль из вьюшек
    удалил тестовый ErrorExample
    
    * добавил комментарии
    
    * добавил View для описания начала теста по направлению Android.
    
    * добавил strip чтобы удалить лишние пробелы, если они будут в шаблонах (лишнии переводы строк)
    
    * Сделал указание пути к статичным файлам в переменной окружения $STATIC_FOLDER
    
    * Сделал указание пути к статичным файлам в переменной окружения $STATIC_FOLDER
    
    * исправление после мерджа
    исправил backend.py в actions.py
    
    * Переименовал types_
    
    * убрал использование codecs

--- androbot/handlers.py
+++ androbot/handlers.py
@@ -1,6 +1,6 @@
 from aiogram import types as aiotypes
 
-from . import backend, states, views
+from . import actions, states, views
 from .errors import UserExistsError
 from .main import bot, dp
 
@@ -15,10 +15,11 @@ async def send_start_screen(message: aiotypes.Message):
     2. Регистрируем событие что пользователь нажал старт
     3. Формируем ответное сообщение и показываем главное меню
     """
-    backend.register_action("start", message)
+    actions.register_action("start", message)
 
     try:
-        backend.add_user(message)
+        actions.add_user(message)
+
         view = views.get_hello_message(message)
         await bot.send_message(
             text=view.text,
@@ -43,10 +44,29 @@ async def send_start_screen(message: aiotypes.Message):
     )
 
 
-@dp.message_handler(text="Android Developer", state=states.MainDialogueStates.MAIN_MENU)
+@dp.message_handler(text="Android Developer", state=states.DialogueStates.MAIN_MENU)
+async def show_android_developer_init(message: aiotypes.Message):
+    """
+    Обработчик для кнопки выбора специализации Android developer
+    """
+
+    state = dp.current_state(user=message.from_user.id)
+    await state.set_state("android_developer_init_view")
+
+    view = views.get_android_developer_init_view(message)
+
+    await bot.send_message(
+        text=view.text,
+        chat_id=message.chat.id,
+        parse_mode=aiotypes.ParseMode.MARKDOWN,
+        reply_markup=view.markup,
+    )
+
+
+@dp.message_handler(text="Готов!", state=states.DialogueStates.ANDROID_DEVELOPER_INIT_VIEW)
 async def show_first_question(message: aiotypes.Message):
     """
-    Обработчик для кнопки выбора специализации
+    Зададем вопросы
     """
 
     state = dp.current_state(user=message.from_user.id)
@@ -57,7 +77,36 @@ async def show_first_question(message: aiotypes.Message):
     await bot.send_message(message.chat.id, text_answer)
 
 
-@dp.message_handler(state="*")
-async def echo(message: aiotypes.Message):
-    """Хэндлер просто отвечает сообщением, которое было прислано"""
-    await message.answer(message.text)
+@dp.message_handler(text="Отмена", state=states.DialogueStates.ANDROID_DEVELOPER_INIT_VIEW)
+async def back_to_main_menu(message: aiotypes.Message):
+    """
+    Возвращаемся в главное меню
+    """
+
+    state = dp.current_state(user=message.from_user.id)
+    await state.set_state("main_menu")
+
+    view = views.get_main_menu(message)
+
+    await bot.send_message(
+        text=view.text,
+        chat_id=message.chat.id,
+        parse_mode=aiotypes.ParseMode.MARKDOWN,
+        reply_markup=view.markup,
+    )
+
+
+@dp.message_handler(state=states.DialogueStates.FIRST_QUESTION)
+async def check_first_question(message: aiotypes.Message):
+    """
+    Проверяем ответ на тестовый вопрос
+    """
+    user_answer = message.text
+
+    if user_answer == "42":
+        await message.reply("Да ты шаришь в теме")
+        state = dp.current_state(user=message.from_user.id)
+        await state.set_state("main_menu")
+
+    else:
+        await message.reply("Неверно, подумайте еще!", reply=False)
--- androbot/views.py
+++ androbot/views.py
@@ -1,8 +1,9 @@
 import aiogram.types as aiotypes
 
-from .androbot_types import View
+from . import actions
 from .config import settings
 from .templates import render_message
+from .types_ import View
 
 
 def get_main_menu(message: aiotypes.Message) -> View:
@@ -11,11 +12,13 @@ def get_main_menu(message: aiotypes.Message) -> View:
     """
     template_file = settings.static_folder / "start.md"
 
-    with open(template_file, "r", encoding="utf-8") as f:
+    with open(template_file, encoding="utf-8") as f:
         answer_text = f.read()
 
-    btn_1 = aiotypes.KeyboardButton("Android Developer")
-    reply_kb = aiotypes.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True).add(btn_1)
+    reply_kb = aiotypes.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    for speciality in actions.get_main_menu():
+        btn_1 = aiotypes.KeyboardButton(speciality)
+        reply_kb.add(btn_1)
 
     return View(render_message(answer_text, message), reply_kb)
 
@@ -26,7 +29,27 @@ def get_hello_message(message: aiotypes.Message) -> View:
     """
     template_file = settings.static_folder / "hello.md"
 
-    with open(template_file, "r", encoding="utf-8") as f:
+    with open(template_file, encoding="utf-8") as f:
         answer_text = f.read()
 
     return View(render_message(answer_text, message))
+
+
+def get_android_developer_init_view(message: aiotypes.Message) -> View:
+    """
+    Возвращает View стартового экрана тестирования по специальности Андроид разработчик
+    """
+    template_file = settings.static_folder / "android_developer.md"
+
+    with open(template_file, encoding="utf-8") as f:
+        answer_text = f.read()
+
+    btn_1 = aiotypes.KeyboardButton("Готов!")
+    btn_2 = aiotypes.KeyboardButton("Отмена")
+    reply_kb = (
+        aiotypes.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+        .row()
+        .add(btn_1, btn_2)
+    )
+
+    return View(render_message(answer_text, message), reply_kb)
