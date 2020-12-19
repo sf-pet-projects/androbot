6721ec5b6cf91ad6f98ac42ee46da0546583e00c
rzabolotin <rzabolotin@gmail.com>
2020-12-20 02:17:04 +1000

    Добавил скрипт для загрузки вопросов в пустую базу

--- androbot/load_questions.py
+++ androbot/load_questions.py
@@ -5,10 +5,11 @@ from androbot.actions import Actions
 from androbot.database import engine
 from androbot.types_ import Specialty
 
-models.Base.metadata.create_all(bind=engine)
-
 if len(sys.argv) < 2:
     print("You're must specify filename in command line")
+    sys.exit(1)
+
+models.Base.metadata.create_all(bind=engine)
 
 with Actions() as act:
     act.load_questions(specialty=Specialty.ANDROID, file=sys.argv[1])
