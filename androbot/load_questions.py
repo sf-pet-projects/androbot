6c84d05452bde47194518139cd073dbdd52be7c9
Roman Zabolotin <rzabolotin@gmail.com>
2020-12-25 12:02:33 +1000

    Load questions with argparse (#27)
    * Сделал скрипт загрузки сообщений через argparse

--- androbot/load_questions.py
+++ androbot/load_questions.py
@@ -1,15 +1,52 @@
-import sys
+import argparse
+import os
 
 from androbot import models
 from androbot.actions import Actions
 from androbot.database import engine
 from androbot.types_ import Specialty
 
-if len(sys.argv) < 2:
-    print("You're must specify filename in command line")
-    sys.exit(1)
 
-models.Base.metadata.create_all(bind=engine)
+def load_questions(speciality: Specialty, filename: str, drop_questions: bool) -> None:
+    """
+    Загружаем в базу данных вопросы из указанного csv файла.
+    Указываем специальность для которой нужно загрузить вопросы
+    Если указан параметр drop_questions - то удаляем старые
+    вопросы перед загрузкой (если нет еще ответов - сработает)
+    """
 
-with Actions() as act:
-    act.load_questions(specialty=Specialty.ANDROID, file=sys.argv[1])
+    models.Base.metadata.create_all(bind=engine)
+
+    with Actions() as act:
+
+        if drop_questions:
+            act.remove_questions(speciality.value)
+
+        act.load_questions(specialty=speciality, file=filename)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="csv file with questions", type=str)
+    parser.add_argument(
+        "-s",
+        "--speciality",
+        choices=[s.value for s in Specialty],
+        default=Specialty.ANDROID,
+    )
+    parser.add_argument(
+        "-d", "--drop_questions", help="drop questions before load", action="store_true"
+    )
+    args = parser.parse_args()
+
+    if not os.path.exists(args.filename):
+        print(f"File '{args.filename}' doesn't exist")
+        return
+
+    speciality = Specialty(args.speciality)
+
+    load_questions(speciality, args.filename, args.drop_questions)
+
+
+if __name__ == "__main__":
+    main()
