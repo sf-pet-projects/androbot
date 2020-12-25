5c107a96d350c2644b9a8bd297b9226cbc3fd7e3
Roman Zabolotin <rzabolotin@gmail.com>
2020-12-25 12:02:07 +1000

    None для fsm_redis_host
    Co-authored-by: Nikita Churikov <8545082+churnikov@users.noreply.github.com>

--- androbot/config.py
+++ androbot/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     db_port: int = Field(5432, env="DB_PORT")
     db_name: str = Field(..., env="DB_NAME")
     static_folder: Path = Field("templates", env="STATIC_FOLDER")
-    fsm_redis_host: str = Field("", env="REDIS_HOST")
+    fsm_redis_host: Optional[str] = Field(None, env="REDIS_HOST")
     fsm_redis_port: int = Field(6379, env="REDIS_PORT")
     fsm_redis_db: int = Field(5, env="REDIS_DB")
     fsm_redis_password: str = Field("", env="REDIS_PASSWORD")
